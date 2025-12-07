a3a2cb1ccd4fc0e343d858168fa76b04286ae0d3
Kirill Chernyshov <kirill.chernyshov@firstbeatmedia.com>
2025-12-07 15:26:54 +0400

    no need keeping whole beams trace (keep only previous line)

--- aoc2025/Day07/aoc2025-day07-code.php
+++ aoc2025/Day07/aoc2025-day07-code.php
@@ -10,47 +10,46 @@ $src = STDIN;
 stream_set_blocking(STDIN, false);
 $res1 = 0;
 $res2 = 0;
-
-$beams = [];
-$row = 0;
+$prev_beams = [];
 do
 {
     $line = trim(fgets($src));
     if ($line)
     {
+        $beams = [];
         for ($i = 0; $i < strlen($line); $i++)
         {
             // - the entrance of the first beam
             if ($line[$i] == 'S')
             {
-                $beams[$row][$i] = 1;
+                $beams[$i] = 1;
             }
             else
             {
-                // - take number of beams from the cell above, if any
-                $beams[$row][$i] = ($beams[$row][$i] ?? 0) + ($beams[$row - 1][$i] ?? 0);
+                // - increase the number of beams with the value coming from above, if any
+                $beams[$i] = ($beams[$i] ?? 0) + ($prev_beams[$i] ?? 0);
 
                 // - if there's a splitter in the current cell
                 if ($line[$i] == '^')
                 {
                     // - splitter increases the cells to the left and to the right. if there's a beam coming from above
-                    if ($beams[$row - 1][$i] > 0) {
+                    if (($prev_beams[$i] ?? 0) > 0) {
                         $res1++; // - count any split of existing beam(s)
-                        $beams[$row][$i - 1] = ($beams[$row][$i - 1] ?? 0) + $beams[$row - 1][$i];
-                        $beams[$row][$i + 1] = ($beams[$row][$i + 1] ?? 0) + $beams[$row - 1][$i];
+                        $beams[$i - 1] = ($beams[$i - 1] ?? 0) + $prev_beams[$i];
+                        $beams[$i + 1] = ($beams[$i + 1] ?? 0) + $prev_beams[$i];
                     }
                     // - current cell becomes zero (splitter does not pass the beam down)
-                    $beams[$row][$i] = 0;
+                    $beams[$i] = 0;
                 }
             }
         }
 
-        $row++;
+        $prev_beams = $beams;
     }
 }
 while (!feof($src));
 
 // - count the total beams number in the bottom
-$res2 = array_sum($beams[count($beams) - 1]);
+$res2 = array_sum($beams);
 
 echo "RES: $res1, $res2\n";
